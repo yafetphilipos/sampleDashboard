876f51d6dcc3548c5cf2cd4488c915bda6d34ab3
yafet <yafetbabi@gmail.com>
2024-08-20 11:57:55 +0300

    basic dashboard item added

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,9 +1,10 @@
-// import bcrypt from 'bcrypt';
-// import { db } from '@vercel/postgres';
-// import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+import bcrypt from 'bcrypt';
+import { db } from '@vercel/postgres';
+import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
 // const client = await db.connect();
 
+// // console.log(client)
 // async function seedUsers() {
 //   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 //   await client.sql`
